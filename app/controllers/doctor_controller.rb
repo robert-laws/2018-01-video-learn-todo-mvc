d3955f8a8aacc53a0502e31312a50fd4a92c4381
robert-laws <roblaws@me.com>
2018-01-21 06:49:11 +0300

    add Doctor model, controller, view stubs & migrate/seed db

---
+++ app/controllers/doctor_controller.rb
@@ -0,0 +1,10 @@
+class DoctorController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
---
+++ test/controllers/doctor_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class DoctorControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get doctor_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get doctor_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get doctor_new_url
+    assert_response :success
+  end
+
+end
