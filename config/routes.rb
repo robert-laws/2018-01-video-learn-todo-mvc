0da60a0b3681c7217e4f5b5d825c9f711e5e1686
robert-laws <roblaws@me.com>
2018-01-21 07:02:09 +0300

    rename folders/files to correct errors. add basic mvc setup/functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :doctor, only: [:index, :show, :new]
+  resources :doctors, only: [:index, :show, :new]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
