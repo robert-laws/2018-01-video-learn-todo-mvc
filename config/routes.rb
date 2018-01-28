a8af858fce240743c93933cbd42a1db5125fc0c7
robert-laws <roblaws@me.com>
2018-01-28 06:44:59 +0300

    set up appointments and patients model/controllers/migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get 'patients/index'
+
+  get 'patients/show'
+
+  get 'patients/new'
+
+  get 'patients/edit'
+
+  get 'appointments/index'
+
+  get 'appointments/show'
+
+  get 'appointments/new'
+
+  get 'appointments/edit'
+
   resources :doctors, only: [:index, :show, :new]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180121034534) do
+ActiveRecord::Schema.define(version: 20180128034241) do
+
+  create_table "appointments", force: :cascade do |t|
+    t.datetime "date"
+    t.integer "doctor_id"
+    t.integer "patient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
 
   create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
